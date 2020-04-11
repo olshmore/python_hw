e71a5a23f40941c8af5c47da1d11163a06be61d3
Olga Shaposhnikova <olshmore@gmail.com>
2020-04-11 15:06:52 +0300

    task 1

---
+++ 1/1_1.py
@@ -0,0 +1,7 @@
+name=input('Hi! What is your name?')
+print('Welcome, ' , name)
+
+input('Do you like maths?')
+
+p=int(input('Whatever, type integer number'))
+print(p+10)
---
+++ 1/1_3.py
@@ -0,0 +1,6 @@
+n=(input('Type in digit'))
+
+three=int(n+n+n)
+two=int(n+n)
+one=int(n)
+print(one+two+three)
